1a6df831efed7b4a122a8c712fd6c236c1057d8c
RWKrause <43546240+RWKrause@users.noreply.github.com>
2020-05-27 13:27:35 +0200

    Update bergmM.R
    adding offset()

--- R/bergmM.R
+++ R/bergmM.R
@@ -33,6 +33,9 @@
 #'
 #' @param startVals vector;
 #' optional starting values for the parameter estimation. 
+#'
+#'@param offset.coef vector;
+#'A vector of coefficients for the offset terms.
 #' 
 #' @param nImp count;
 #' number of imputed networks to be returned. If null, no imputed network will be returned.
@@ -85,156 +88,157 @@
 #'}
 #' @export
 
-bergmM <- function(formula,
-                   burn.in       = 100,
-                   main.iters    = 1000,
-                   aux.iters     = 1000,
-                   prior.mean    = NULL,
-                   prior.sigma   = NULL,
-                   nchains       = 3,
-                   gamma         = 0.5,
-                   V.proposal    = 0.0025,
-                   seed          = NULL,
-                   startVals     = NULL,
-                   nImp          = NULL,
+bergmM <- function(formula, 
+                   burn.in = 100, 
+                   main.iters = 1000, 
+                   aux.iters = 1000,
+                   prior.mean = NULL, 
+                   prior.sigma = NULL, 
+                   nchains = NULL, 
+                   gamma = 0.5,
+                   V.proposal = 0.0025, 
+                   seed = NULL, 
+                   startVals = NULL,  
+                   offset.coef = NULL,
+                   nImp = NULL,
                    missingUpdate = NULL,
-                   ...) {
-  
-    if (is.null(seed)) set.seed(sample(1:999, 1)) else set.seed(seed)
-
-    y      <- ergm.getnetwork(formula)
-    model  <- ergm_model(formula, y)
-    sy     <- summary(formula)
-    dim    <- length(sy)
-    
-    if (dim == 1) stop("Model dimension must be greater than 1")
-
-    impNets <- NULL
-
-    if (!is.null(nImp)) {
-      nImp <- max(0, min(nImp, main.iters))
-      thinImp <- as.integer(main.iters / nImp)
-      impIter <- 1
-      impNets <- vector("list", nImp)
+                   ...)
+{
+  if (is.null(seed))
+    set.seed(sample(1:999, 1))
+  else set.seed(seed)
+  y <- ergm.getnetwork(formula)
+  model <- ergm_model(formula, y)
+  sy <- summary(formula)
+  dim <- length(sy)
+  if (dim == 1) {stop("Model dimension must be greater than 1")}
+  if (!is.null(offset.coef)) {
+    if (any(offset.coef %in% c(Inf,-Inf,NaN,NA))) {
+      stop("Inf,-Inf,NaN,NA are not allowed for offset.coef. \n If Inf or -Inf are required use large values instead (e.g., 1000 or -1000).")
     }
-
-    missingTies <- matrix(0, y$gal$n, y$gal$n)
-    missingTies[is.na(as.matrix.network(y))] <- 1
-    missingTies <- as.edgelist(as.network(missingTies), n = y$gal$n)
-
-    if (is.null(missingUpdate)) {missingUpdate <- sum(is.na(as.matrix.network(y)))}
-
-    Clist <- ergm.Cprepare(y, model)
-
-    control <- control.ergm(MCMC.burnin = aux.iters,
-                            MCMC.interval = 1,
-                            MCMC.samplesize = 1)
-
-    proposal <- ergm_proposal(object = ~.,
-                              constraints = ~.,
-                              arguments = control$MCMC.prop.args,
-                              nw = y)
-
-    if (is.null(prior.mean)) prior.mean <- rep(0, dim)
-    if (is.null(prior.sigma)) prior.sigma <- diag(100, dim, dim)
-    if (is.null(nchains)) nchains <- 2 * dim
-
-    S.prop <- diag(V.proposal, dim, dim)
-    Theta <- array(NA, c(main.iters, dim, nchains))
-
-    # starting values
-    if (is.null(startVals)) {
-      suppressMessages( mple <- ergm(formula, estimate = "MPLE", verbose = FALSE)$coef )
-      theta <- matrix( mple + runif(dim * nchains, min = -0.1, max = 0.1), dim, nchains)
-    } else {
-      theta <- matrix( startVals + runif(dim * nchains, min = -0.1, max = 0.1), dim, nchains)
-    }
-
-    acc.counts <- rep(0L, nchains)
-    theta1 <- rep(NA, dim)
-    tot.iters <- burn.in + main.iters
-
-    impNet <- y
-
-    f <- as.character(formula)
-    currentFormula <- formula(paste("impNet",
-                                    f[3:length(f)], sep = " ~ "))
-
-    clock.start <- Sys.time()
-    
-    message(" > MCMC start")
-
-    for (k in 1:tot.iters) {
-        for (h in 1:nchains) {
-
-            theta1 <- theta[, h] + 
-            gamma * apply(theta[, sample(seq(1, nchains)[-h], 2)], 1, diff) + 
-            rmvnorm(1, sigma = S.prop)[1, ]
-
-            delta <- ergm_MCMC_slave(Clist = Clist,
-                                     proposal = proposal,
-                                     eta = theta1,
-                                     control = control,
-                                     verbose = FALSE)$s
-
-            pr <- dmvnorm(rbind(theta1, theta[, h]),
-                          mean = prior.mean,
-                          sigma = prior.sigma,
-                          log = TRUE)
-
-            beta <- (theta[, h] - theta1) %*% t(delta) + pr[1] - pr[2]
-
-            if (beta >= log(runif(1))) {
-              theta[, h] <- theta1
-
-              if (k > burn.in) { acc.counts[h] <- acc.counts[h] + 1 }
-
-              # Network Imputation
-              impNet <- simulate(currentFormula, 
-                                 coef = theta1,
-                                 output = 'network',
-                                 basis = y, 
-                                 constraints = ~fixallbut(missingTies),
-                                 nsim = 1,
-                                 control = control.simulate(MCMC.burnin = missingUpdate))
-
-              y2 <- ergm.getnetwork(currentFormula)
-              model2 <- ergm_model(currentFormula, y2)
-              Clist <- ergm.Cprepare(y2, model2)
-            }
-          }
-
-        if (k > burn.in) Theta[k - burn.in, , ] <- theta
-
-         if (!is.null(nImp)) {
-          if ((k - burn.in) == impIter * thinImp) {
-            impNets[[impIter]] <- impNet
-            impIter <- impIter + 1
-          }
-        }
-    }
-
-    clock.end <- Sys.time()
-    runtime   <- difftime(clock.end, clock.start)
-
-    clock.end <- Sys.time()
-    runtime   <- difftime(clock.end, clock.start)
-    
-    FF <- mcmc(apply(Theta, 2, cbind))
-    AR <- round(1 - rejectionRate(FF)[1], 2)
-    names(AR) <- NULL
-    ess <- round(effectiveSize(FF), 0)
-    names(ess) <- model$coef.names
-    
-    out = list(Time    = runtime,
-               formula = formula,
-               specs   = model$coef.names,
-               dim     = dim,
-               Theta   = FF,
-               AR      = AR,
-               ess     = ess,
-               impNets = impNets)
-
-    class(out) <- "bergm"
-    return(out)
   }
+  impNets <- NULL
+  if (!is.null(nImp)) {
+    nImp <- max(0, min(nImp, main.iters))
+    thinImp <- as.integer(main.iters/nImp)
+    impIter <- 1
+    impNets <- vector("list", nImp)
+  }
+  missingTies <- matrix(0, y$gal$n, y$gal$n)
+  missingTies[is.na(as.matrix.network(y))] <- 1
+  missingTies <- as.edgelist(as.network(missingTies), n = y$gal$n)
+  if (is.null(missingUpdate)) {
+    missingUpdate <- sum(is.na(as.matrix.network(y)))
+  }
+  Clist <- ergm.Cprepare(y, model)
+  control <- control.ergm(MCMC.burnin = aux.iters, MCMC.interval = 1,
+                          MCMC.samplesize = 1)
+
+  if (!is.null(control$init)) {
+    if (length(control$init) != length(model$etamap$offsettheta)) {
+      if (verbose) {
+        message("control$init =")
+        message_print(control$init)
+        message("number of statistics is ", length(model$coef.names),
+                "")
+      }
+      stop(paste("Invalid starting parameter vector control$init:",
+                 "wrong number of parameters.", "If you are passing output from another ergm run as control$init,",
+                 "in a model with curved terms, see help(enformulate.curved)."))
+    }
+  } else {control$init <- rep(NA, length(model$etamap$offsettheta))}
+  if (!is.null(offset.coef)) {
+    if (length(control$init[model$etamap$offsettheta]) !=
+        length(offset.coef)) {
+      stop("Invalid offset parameter vector offset.coef: ",
+           "wrong number of parameters: expected ",
+           length(control$init[model$etamap$offsettheta]),
+           " got ", length(offset.coef), ".")}
+    control$init[model$etamap$offsettheta] <- offset.coef
+  }
+  if (any(is.na(control$init) & model$etamap$offsettheta)) {
+    stop("The model contains offset terms whose parameter values have not been specified:",
+         paste.and(model$coef.names[is.na(control$init) |
+                                model$offsettheta]), ".", sep = "") }
+
+
+  proposal <- ergm_proposal(object = ~., constraints = ~.,
+                            arguments = control$MCMC.prop.args, nw = y)
+  if (is.null(prior.mean))
+    prior.mean <- rep(0, dim)
+  if (is.null(prior.sigma))
+    prior.sigma <- diag(100, dim, dim)
+  if (is.null(nchains))
+    nchains <- 2 * dim
+  S.prop <- diag(V.proposal, dim, dim)
+  Theta <- array(NA, c(main.iters, dim, nchains))
+  if (is.null(startVals)) {
+    suppressMessages(mple <- ergm(formula, estimate = "MPLE",
+                                  verbose = FALSE,
+                                  offset.coef = offset.coef)$coef)
+    theta <- matrix(mple + runif(dim * nchains, min = -0.1,
+                                 max = 0.1), dim, nchains)
+  } else {
+    theta <- matrix(startVals + runif(dim * nchains, min = -0.1,
+                                      max = 0.1), dim, nchains)
+  }
+  theta[model$etamap$offsettheta,] <- offset.coef
+  acc.counts <- rep(0L, nchains)
+  theta1 <- rep(NA, dim)
+  tot.iters <- burn.in + main.iters
+  impNet <- y
+  f <- as.character(formula)
+  currentFormula <- formula(paste("impNet", f[3:length(f)],
+                                  sep = " ~ "))
+  clock.start <- Sys.time()
+  message(" > MCMC start")
+  for (k in 1:tot.iters) {
+    for (h in 1:nchains) {
+      theta1 <- theta[, h] + gamma * apply(theta[, sample(seq(1,
+                  nchains)[-h], 2)], 1, diff) + rmvnorm(1, sigma = S.prop)[1,]
+
+      theta1[model$etamap$offsettheta] <- offset.coef
+
+      delta <- ergm_MCMC_slave(Clist = Clist, proposal = proposal,
+                               eta = theta1, control = control,
+                               verbose = FALSE)$s
+      pr <- dmvnorm(rbind(theta1, theta[, h]), mean = prior.mean,
+                    sigma = prior.sigma, log = TRUE)
+      beta <- (theta[, h] - theta1) %*% t(delta) + pr[1] -
+        pr[2]
+      if (beta >= log(runif(1))) {
+        theta[, h] <- theta1
+        if (k > burn.in) {
+          acc.counts[h] <- acc.counts[h] + 1
+        }
+        impNet <- simulate(currentFormula, coef = theta1,
+                           output = "network", basis = y, constraints = ~fixallbut(missingTies),
+                           nsim = 1, control = control.simulate(MCMC.burnin = missingUpdate))
+        y2 <- ergm.getnetwork(currentFormula)
+        model2 <- ergm_model(currentFormula, y2)
+        Clist <- ergm.Cprepare(y2, model2)
+      }
+    }
+    if (k > burn.in)
+      Theta[k - burn.in, , ] <- theta
+    if (!is.null(nImp)) {
+      if ((k - burn.in) == impIter * thinImp) {
+        impNets[[impIter]] <- impNet
+        impIter <- impIter + 1
+      }
+    }
+  }
+  clock.end <- Sys.time()
+  runtime <- difftime(clock.end, clock.start)
+  clock.end <- Sys.time()
+  runtime <- difftime(clock.end, clock.start)
+  FF <- mcmc(apply(Theta, 2, cbind))
+  AR <- round(1 - rejectionRate(FF)[1], 2)
+  names(AR) <- NULL
+  ess <- round(effectiveSize(FF), 0)
+  names(ess) <- model$coef.names
+  out = list(Time = runtime, formula = formula, specs = model$coef.names,
+             dim = dim, Theta = FF, AR = AR, ess = ess, impNets = impNets)
+  class(out) <- "bergm"
+  return(out)
+}
