2af6b31444b0f6fb32698a31da47eb254395127e
RWKrause <43546240+RWKrause@users.noreply.github.com>
2020-05-27 13:24:48 +0200

    Update bergm.R
    adding offset()

--- R/bergm.R
+++ R/bergm.R
@@ -40,6 +40,9 @@
 #' @param startVals vector;
 #' optional starting values for the parameter estimation. 
 #'
+#'@param offset.coef vector;
+#'A vector of coefficients for the offset terms.
+#'
 #' @param ... additional arguments, to be passed to lower-level functions.
 #'
 #' @references
@@ -65,103 +68,108 @@
 #'
 #' @export
 
-bergm <- function(formula,
-                  prior.mean  = NULL,
-                  prior.sigma = NULL,
-                  burn.in     = 100,
-                  main.iters  = 1000,
-                  aux.iters   = 1000,
-                  nchains     = NULL,
-                  gamma       = 0.5,
-                  V.proposal  = 0.0025,
-                  startVals   = NULL,
-                  ...){
-
-  y     <- ergm.getnetwork(formula)
+bergm <- function(formula, prior.mean = NULL, prior.sigma = NULL, burn.in = 100,
+          main.iters = 1000, aux.iters = 1000, nchains = NULL, gamma = 0.5,
+          V.proposal = 0.0025, startVals = NULL, offset.coef = NULL, ...) {
+  y <- ergm.getnetwork(formula)
   model <- ergm_model(formula, y)
-  sy    <- summary(formula)
-  dim   <- length(sy)
-  
-  if (dim == 1) stop("Model dimension must be greater than 1")
-  if (any(is.na(as.matrix.network(y)))) print("Network has missing data. Use bergmM() instead.")
-  
-  # For network simulation
-  Clist   <- ergm.Cprepare(y, model)
-  
-  control <- control.ergm(MCMC.burnin = aux.iters,
-                          MCMC.interval = 1,
+  sy <- summary(formula)
+  dim <- length(sy)
+  if (dim == 1)
+    stop("Model dimension must be greater than 1")
+  if (any(is.na(as.matrix.network(y))))
+    print("Network has missing data. Use bermM() instead.")
+  if (!is.null(offset.coef)) {
+    if (any(offset.coef %in% c(Inf,-Inf,NaN,NA))) {
+    stop("Inf,-Inf,NaN,NA are not allowed for offset.coef. \n If Inf or -Inf are required use large values instead (e.g., 1000 or -1000).")
+    }
+  }
+  Clist <- ergm.Cprepare(y, model)
+  control <- control.ergm(MCMC.burnin = aux.iters, MCMC.interval = 1,
                           MCMC.samplesize = 1)
-  
-  proposal <- ergm_proposal(object = ~.,
-                            constraints = ~.,
-                            arguments = control$MCMC.prop.args,
-                            nw = y)
 
-  if (is.null(prior.mean))  prior.mean  <- rep(0, dim)
-  if (is.null(prior.sigma)) prior.sigma <- diag(100, dim, dim)
-  if (is.null(nchains)) nchains <- 2 * dim
-  
-  S.prop <- diag(V.proposal, dim, dim)
-  
-  Theta <- array(NA, c(main.iters, dim, nchains))
 
-  # starting values
-  if (is.null(startVals)) {
-    suppressMessages( mple <- ergm(formula, estimate = "MPLE", verbose = FALSE)$coef )
-    theta <- matrix( mple + runif(dim * nchains, min = -0.1, max = 0.1), dim, nchains)
-  } else {
-    theta <- matrix( startVals + runif(dim * nchains, min = -0.1, max = 0.1), dim, nchains)
-  }
-
-  theta1     <- rep(NA, dim)
-  tot.iters  <- burn.in + main.iters
-  
-  clock.start <- Sys.time()
-
-  message(" > MCMC start")
-  
-  for (k in 1:tot.iters) {
-      for (h in 1:nchains) {
-
-          theta1 <- theta[, h] + 
-                    gamma * apply(theta[, sample(seq(1, nchains)[-h], 2)], 1, diff) + 
-                    rmvnorm(1, sigma = S.prop)[1, ]
-          
-          delta <- ergm_MCMC_slave(Clist = Clist,
-                                   proposal = proposal,
-                                   eta = theta1,
-                                   control = control,
-                                   verbose = FALSE)$s
-          
-          pr <- dmvnorm(rbind(theta1, theta[, h]),
-                        mean = prior.mean,
-                        sigma = prior.sigma,
-                        log = TRUE)
-            
-          beta <- (theta[, h] - theta1) %*% t(delta) + pr[1] - pr[2]
-
-          if (beta >= log(runif(1))) theta[, h] <- theta1
+  if (!is.null(control$init)) {
+    if (length(control$init) != length(model$etamap$offsettheta)) {
+      if (verbose) {
+        message("control$init =")
+        message_print(control$init)
+        message("number of statistics is ", length(model$coef.names),
+                "")
       }
-      if (k > burn.in) Theta[k - burn.in, , ] <- theta
+      stop(paste("Invalid starting parameter vector control$init:",
+                 "wrong number of parameters.", "If you are passing output from another ergm run as control$init,",
+                 "in a model with curved terms, see help(enformulate.curved)."))
+    }
+  } else {control$init <- rep(NA, length(model$etamap$offsettheta))}
+  if (!is.null(offset.coef)) {
+    if (length(control$init[model$etamap$offsettheta]) !=
+        length(offset.coef)) {
+      stop("Invalid offset parameter vector offset.coef: ",
+           "wrong number of parameters: expected ",
+           length(control$init[model$etamap$offsettheta]),
+           " got ", length(offset.coef), ".")}
+    control$init[model$etamap$offsettheta] <- offset.coef
   }
+  if (any(is.na(control$init) & model$etamap$offsettheta)) {
+    stop("The model contains offset terms whose parameter values have not been specified:",
+         paste.and(model$coef.names[is.na(control$init) |
+                                      model$offsettheta]), ".", sep = "") }
 
+
+  proposal <- ergm_proposal(object = ~., constraints = ~.,
+                            arguments = control$MCMC.prop.args, nw = y)
+  if (is.null(prior.mean))
+    prior.mean <- rep(0, dim)
+  if (is.null(prior.sigma))
+    prior.sigma <- diag(100, dim, dim)
+  if (is.null(nchains))
+    nchains <- 2 * dim
+  S.prop <- diag(V.proposal, dim, dim)
+  Theta <- array(NA, c(main.iters, dim, nchains))
+  if (is.null(startVals)) {
+    suppressMessages(mple <- ergm(formula, estimate = "MPLE",
+                                  verbose = FALSE,
+                                  offset.coef = offset.coef)$coef)
+    theta <- matrix(mple + runif(dim * nchains, min = -0.1,
+                                 max = 0.1), dim, nchains)
+  }  else {
+    theta <- matrix(startVals + runif(dim * nchains, min = -0.1,
+                                      max = 0.1), dim, nchains)
+  }
+  theta[model$etamap$offsettheta,] <- offset.coef
+  theta1 <- rep(NA, dim)
+  tot.iters <- burn.in + main.iters
+  clock.start <- Sys.time()
+  message(" > MCMC start")
+  for (k in 1:tot.iters) {
+    for (h in 1:nchains) {
+      theta1 <- theta[, h] + gamma * apply(theta[, sample(seq(1,
+                    nchains)[-h], 2)], 1, diff) + rmvnorm(1, sigma = S.prop)[1,]
+
+      theta1[model$etamap$offsettheta] <- offset.coef
+
+      delta <- ergm_MCMC_slave(Clist = Clist, proposal = proposal,
+                               eta = theta1, control = control, verbose = FALSE)$s
+      pr <- dmvnorm(rbind(theta1, theta[, h]), mean = prior.mean,
+                    sigma = prior.sigma, log = TRUE)
+      beta <- (theta[, h] - theta1) %*% t(delta) + pr[1] -
+        pr[2]
+      if (beta >= log(runif(1)))
+        theta[, h] <- theta1
+    }
+    if (k > burn.in)
+      Theta[k - burn.in, , ] <- theta
+  }
   clock.end <- Sys.time()
-  runtime   <- difftime(clock.end, clock.start)
-  
+  runtime <- difftime(clock.end, clock.start)
   FF <- mcmc(apply(Theta, 2, cbind))
   AR <- round(1 - rejectionRate(FF)[1], 2)
   names(AR) <- NULL
   ess <- round(effectiveSize(FF), 0)
   names(ess) <- model$coef.names
-  
-  out = list(Time    = runtime,
-             formula = formula,
-             specs   = model$coef.names,
-             dim     = dim,
-             Theta   = FF,
-             AR      = AR,
-             ess     = ess)
-
+  out = list(Time = runtime, formula = formula, specs = model$coef.names,
+             dim = dim, Theta = FF, AR = AR, ess = ess)
   class(out) <- "bergm"
   return(out)
 }
