6685b851194663d8e385a7f248e8a8138354b345
Robert Krause <43546240+RWKrause@users.noreply.github.com>
2024-09-12 15:57:00 -0400

    Update plot.bergm.R

--- R/plot.bergm.R
+++ R/plot.bergm.R
@@ -3,11 +3,11 @@
 #' This function creates  MCMC diagnostic plots for \code{bergm} objects.
 #'
 #' @docType methods
-#' 
+#'
 #' @param x an \code{R} object of class \code{bergm}.
-#' 
+#'
 #' @param ... additional arguments, to be passed to lower-level functions.
-#' 
+#'
 #' @examples
 #' \dontrun{
 #' # Load the florentine marriage network
@@ -23,37 +23,37 @@
 #' # MCMC diagnostics plots:
 #' plot(p.flo)
 #' }
-#' 
+#'
 #' @export
 #' @method plot bergm
- 
+
 plot.bergm <- function(x, ...) {
-  
+
   stopifnot(inherits(x, "bergm"))
-  
+  real_dim <- sum(!x$fixed)
   seqq <- 4
-  par(mfrow = c(min(4, x$dim), 3), 
-      oma   = c(0, 0, 3, 0), 
+  par(mfrow = c(min(4, real_dim), 3),
+      oma   = c(0, 0, 3, 0),
       mar   = c(4, 3, 1.5, 1))
-  
-  for (i in 1:x$dim) {
+
+  for (i in 1:real_dim) {
     if (i %in% c(5, 9, 13)) {
       dev.new()
-      par(mfrow = c(min(4, x$dim - (i - 1)), 3), 
-          oma = c(0, 0, 3, 0), 
+      par(mfrow = c(min(4, real_dim - (i - 1)), 3),
+          oma = c(0, 0, 3, 0),
           mar = c(4, 3, 1.5, 1))
     }
-    plot(density(x$Theta[, i]), 
-         main = "", 
-         axes = FALSE, 
-         xlab = bquote(paste(theta[.(i)], " (", .(x$specs[i]), ")")),
+    plot(density(x$Theta[,!x$fixed][,i]),
+         main = "",
+         axes = FALSE,
+         xlab = bquote(paste(theta[.(i)], " (", .(x$specs[!x$fixed][i]), ")")),
          ylab = "", lwd = 2)
     axis(1)
     axis(2)
-    traceplot(x$Theta[, i], type = "l", xlab = "Iterations", ylab = "")
-    autocorr.plot(x$Theta[, i], auto.layout = FALSE, ...)
-    if (x$dim > 4) seqq <- seq(4, x$dim, 4)
-    if (i %in% union(x$dim, seqq)) {
+    traceplot(x$Theta[,!x$fixed][,i], type = "l", xlab = "Iterations", ylab = "")
+    autocorr.plot(x$Theta[,!x$fixed][,i], auto.layout = FALSE, ...)
+    if (real_dim > 4) seqq <- seq(4, real_dim, 4)
+    if (i %in% union(real_dim, seqq)) {
       title(paste("MCMC output for Model: y ~", x$formula[3]), outer = TRUE)
     }
   }
