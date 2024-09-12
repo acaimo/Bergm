f189938b88ad20619e712fa7b3b7b5b354cffffd
Robert Krause <43546240+RWKrause@users.noreply.github.com>
2024-09-12 15:04:30 -0400

    Update summary.bergm.R

--- R/summary.bergm.R
+++ R/summary.bergm.R
@@ -5,42 +5,61 @@
 #' @docType methods
 #' 
 #' @param object an \code{R} object of class \code{bergm}.
+#'
+#' @param returnTable logical;
+#' By default FALSE. Should the results table be returned.
 #' 
 #' @param ... additional arguments, to be passed to lower-level functions.
 #' 
 #' @export
 #' @method summary bergm
  
-summary.bergm <- function(object, ...) {
-  
+
+summary.bergm <- function(object,
+                          returnTable = FALSE,
+                          ...) {
   x <- object
-  
+
   stopifnot(inherits(x, "bergm"))
 
-  cat("\n", "Posterior Density Estimate for Model: y ~", paste(x$formula[3]), "\n", "\n")
-  
+  cat("\nPosterior Density Estimate for Model: y ~",
+      paste(x$formula[3]),
+      "\n\n")
+
   Theta <- as.mcmc(x$Theta)
   quantiles <- c(0.025, 0.25, 0.5, 0.75, 0.975)
-  
-  statnames <- c("Mean", "SD", "Naive SE", "Time-series SE")
-  varstats <- matrix(nrow = nvar(Theta), ncol = length(statnames), 
-                     dimnames = list(varnames(Theta), statnames))
-  
-  Thetavar <- apply(Theta, 2, var)
-  Thetatsvar <- apply(Theta, 2, function(x) coda::spectrum0.ar(x)$spec)
-  varquant <- t(apply(Theta, 2, quantile, quantiles))
-  
-  varstats[, 1] <- apply(Theta, 2, mean)
-  varstats[, 2] <- sqrt(Thetavar)
-  varstats[, 3] <- sqrt(Thetavar / niter(Theta))
-  varstats[, 4] <- sqrt(Thetatsvar / niter(Theta))
-  
+
+  statnames <- c("Mean", "SD", "Naive SE", "Time-series SE","ESS",
+                 "Pr(Theta<0)")
+  varstats <- matrix(nrow = nvar(Theta),
+                     ncol = length(statnames),
+                     dimnames = list(varnames(Theta),
+                                     statnames))
+
+  Thetavar <- apply(Theta[,!x$fixed], 2, var)
+  Thetatsvar <- coda::spectrum0.ar(Theta[,!x$fixed])$spec
+  varquant <- t(apply(Theta[,!x$fixed], 2, quantile, quantiles))
+
+  varstats[, "Mean"] <- apply(Theta, 2, mean)
+  varstats[!x$fixed, "SD"] <- sqrt(Thetavar)
+  varstats[!x$fixed, "Naive SE"] <- sqrt(Thetavar / niter(Theta))
+  varstats[!x$fixed, "Time-series SE"] <- sqrt(Thetatsvar / niter(Theta))
+  varstats[, "ESS"] <- x$ess
+  varstats[!x$fixed, "Pr(Theta<0)"] <- apply(Theta[,!x$fixed], 2,
+                              function(y) {sum(y < 0)/length(y)})
+
   table1 <- drop(varstats)
   table2 <- drop(varquant)
-  
-  rNames <- paste("theta", seq(1, x$dim), " (", x$specs[seq(1, x$dim)], ")", sep = "")
-  
-  rownames(table1) <- rownames(table2) <- rNames
-  print(table1); cat("\n"); print(table2)
+
+  print(table1)
+  cat("\n")
+  print(table2)
   cat("\n", "Acceptance rate:", x$AR, "\n", "\n", "\n")
+  if (any(x$fixed)) {
+    cat("Some parameters were fixed during the estimation:\n",
+        format(row.names(table1)[x$fixed]),'\n\n\n')
+  }
+  if (returnTable) {
+    return(table1)
+  }
 }
